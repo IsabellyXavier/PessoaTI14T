4d6a25f8a06a60bbb0c5bcbd0788e42060f0875e
isabelly.gxavier <isabelly.gxavier@SBC0505245W10-1>
2022-06-06 17:20:08 -0300

    Adicionar arquivos de projeto.

---
+++ PessoaTI14T/DAOPessoa.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace PessoaTI14T
+{
+    class DAOPessoa
+    {
+        public MySqlConnection conexao;
+        public string dados;
+        public string comando;
+        public string resultado;
+        public DAOPessoa()
+        {
+            conexao = new MySqlConnection("server=localhost;Database=NovoPessoa;Uid=root;password=");
+            try
+            {
+
+                conexao.Open();//tentando conectar ao BD
+                MessageBox.Show("Conectado com Sucesso!");
+
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show("Algo deu Errado!\n\n" + erro);// enviando a mensagem de erro aos usuarios.
+                conexao.Close();// fechando a conexao com o banco de dados
+            }
+
+
+        }// fim do Construtor.
+
+//_________________________________________________________________________________________________________________________________________________
+
+        public void Inserir(long cpf, string nome, string telefone, string endereco)
+        {
+            try
+            {
+
+                //Preparar os dados para inserir no banco.
+                dados = "('','" + cpf + "','" + nome + "','" + telefone + "','" + endereco + "')";
+                comando = "Insert into PessoaTI14T(codigo,cpf,nome,telefone,endereco) values " + dados;
+
+                //executar o comando na base de dados.
+                MySqlCommand sql = new MySqlCommand(comando, conexao);
+                resultado = "" + sql.ExecuteNonQuery();
+                MessageBox.Show(resultado + "linha afetada!");
+
+            }
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("Algo deu Errado!\n\n" + erro);// enviando a mensagem de erro aos usuarios.
+
+            }
+        }// fim do metodo inserir.
+
+
+
+    }// fim da classe
+
+}// fim do projeto
---
+++ PessoaTI14T/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PessoaTI14T
+{
+    public partial class Form1 : Form
+    {
+        DAOPessoa pessoa;
+        public Form1()
+        {
+            InitializeComponent();
+            pessoa = new DAOPessoa();// abrindo a conexao com o banco de dados.
+
+        }// fim do construtor
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            // int codigo = Convert.ToInt32(textBox1.Text);//coletar o dado do campo codigo.
+            
+            string tratamentoCPF = maskedTextBox1.Text.Replace(",", "");
+            tratamentoCPF = tratamentoCPF.Replace("-", "");
+
+            long cpf = Convert.ToInt64(tratamentoCPF);//coletar o dado do campo CPF.
+            string nome = textBox2.Text; //coletar o dado do campo nome.
+            string telefone = maskedTextBox2.Text;//coletar o dado do campo telefone.
+            string endereco = textBox4.Text;//coletar o dado do campo emdereco.
+            // chamar o metodo inserir craido na classe DAOPessoa.
+            pessoa.Inserir(cpf, nome, telefone, endereco);// inserir no banco os dados do formulário.
+                       
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show("" + erro);
+            }
+
+
+        }//fim do botão cadastrar
+
+        private void Consultar_Click(object sender, EventArgs e)
+        {
+
+        }//fim do botão consultar
+
+        private void Atualizar_Click(object sender, EventArgs e)
+        {
+
+        }//fim do botão Atualizar
+
+        private void Excluir_Click(object sender, EventArgs e)
+        {
+
+        }//fim do botão Excluir
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }//textBox de codigo;
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }// maskedTextBox do CPF;
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }// textBox do Nome;
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }// maskedTextBox do Telefone;
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }// textBox de endereco;
+
+    }//fim da classe
+
+}//fim do projeto
